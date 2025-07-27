f6cf30378df459c95edc43d1e453a03b0572d3c7
ririo08 <7778943+ririo08@users.noreply.github.com>
2025-07-27 16:00:04 +0900

    chore: eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,52 @@
 import packageJson from './package.json'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+
+  // @keep-sorted
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxtjs/google-fonts',
+    '@vueuse/nuxt',
+  ],
 
   ssr: false,
+  devtools: { enabled: true },
 
   app: {
-    baseURL: '/mini-clock-overlay/'
+    baseURL: '/mini-clock-overlay/',
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  colorMode: {
+    preference: 'light',
+    fallback: 'light',
   },
 
   runtimeConfig: {
     public: {
-      appVersion: packageJson.version
+      appVersion: packageJson.version,
     },
   },
 
-  modules: [
-    "@nuxt/ui",
-    "@nuxt/eslint",
-    "@vueuse/nuxt",
-    "@nuxtjs/google-fonts",
-  ],
+  future: {
+    compatibilityVersion: 4,
+  },
 
-  colorMode: {
-    preference: 'light'
+  compatibilityDate: '2025-07-27',
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
   },
 
   googleFonts: {
     download: true,
     families: {
       'Roboto+Mono': true,
-      'Martian+Mono': true
-    }
+      'Martian+Mono': true,
+    },
   },
-  
-  future: {
-    compatibilityVersion: 4,
-  },
-  compatibilityDate: "2024-08-30",
 })
